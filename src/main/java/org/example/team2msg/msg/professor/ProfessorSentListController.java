8fc68b9875b85841d5e59a388301f79f1b645a6c
eggzuxi <eggzuxi99@gmail.com>
2024-08-07 10:49:11 +0900

    divideListJH

--- src/main/java/org/example/team2msg/msg/professor/ProfessorSentListController.java
+++ src/main/java/org/example/team2msg/msg/professor/ProfessorSentListController.java
@@ -5,9 +5,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
+import org.example.team2msg.msg.message.MsgDAO;
+import org.example.team2msg.msg.message.MsgVO;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet (value = "/proflist/sent")
 @Log4j2
@@ -15,11 +19,33 @@ public class ProfessorSentListController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/professor/profsentlist.jsp").forward(req, resp);
+
+        HttpSession session = req.getSession();
+
+        ProfessorVO professor = (ProfessorVO)session.getAttribute("professor");
+
+        if (professor == null) {
+            resp.sendRedirect(req.getContextPath() + "/proflogin");
+            return;
+        }
+
+        String pid = professor.getPid();
+
+        try {
+            List<MsgVO> messages = MsgDAO.INSTANCE.getSentMessages(pid);
+            req.setAttribute("messageType", "sent");
+            req.setAttribute("messages", messages);
+            req.setAttribute("professorName", professor.getPid());
+            req.getRequestDispatcher("/WEB-INF/professor/profsentlist.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e);
+
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        log.info("doPost");
     }
 }
