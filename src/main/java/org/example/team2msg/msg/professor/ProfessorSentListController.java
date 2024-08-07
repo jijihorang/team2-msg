63d17aafe7991685705d47ca939b192b6e67587d
eggzuxi <eggzuxi99@gmail.com>
2024-08-07 10:18:35 +0900

    addProfControllerJH

---
+++ src/main/java/org/example/team2msg/msg/professor/ProfessorSentListController.java
@@ -0,0 +1,25 @@
+package org.example.team2msg.msg.professor;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.IOException;
+
+@WebServlet (value = "/proflist/sent")
+@Log4j2
+public class ProfessorSentListController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/professor/profsentlist.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
